43f553a6447656e323d645835333c45d61cab30d
XZ361 <2457307263@qq.com>
2023-09-21 18:13:28 +0800

    ref响应式数据-v3

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,16 @@
     <div>
         <h1>Home</h1>
         <p>{{ name }} -- {{ age }}</p>
-        <button @click="handleClcik">click</button>
-        <button @click="handleClcik2">click2</button>
+        <button @click="handleClcik">click</button><br>
+        <button @click="handleClcik2">click2</button><br>
+        <p>{{ str }} -- {{ num }}</p>
+
+        <button @click="handleClcik3">click3</button>
     </div>
 </template>
 
 <script setup>
+import { ref } from "vue";
 // 定义属性和方法
 // 这种方式虽然可以，但是存在问题；
 let name = '吴老二';
@@ -20,6 +24,15 @@ const handleClcik2 = () => {
   name = 'mr.bl';
   age = 10;
   console.log(name,age);
+}
+// 要解决上述问题，我们需要引入v3中的ref模块,以便实现响应式数据
+let str = ref('hello');
+let num = ref(20);
+const handleClcik3 = () => {
+  // 可以发现str是个对象
+  // console.log(str);
+  str.value = 'hello vite!';
+  num.value = 30;
 }
 </script>
 
