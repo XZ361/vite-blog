fba48c4f83717b3cd7729b11e44e840560b55fa9
XZ361 <2457307263@qq.com>
2023-10-15 14:22:34 +0800

    update:computed&v-for

--- src/components/Compute.vue
+++ src/components/Compute.vue
@@ -1,16 +1,18 @@
 <template>
     <div>
         <h2>{{ name }}</h2>
+        <ul>
+            <li v-for="(item, index) in nums" :key="index">{{ item.name }}</li>
+        </ul>
     </div>
 </template>
 
 <script setup>
-import { ref,reactive,computed } from "vue";
-const name = computed(()=>{
+import { ref, reactive, computed } from "vue";
+const name = computed(() => {
     return '王二狗'
 })
+let nums = [{ name: '33' }, { name: '44' }, { name: '33i' }, { name: '44t' }]
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
