334e179fb153157803aee03f53dcbc19b952b010
XZ361 <2457307263@qq.com>
2023-10-15 14:09:04 +0800

    update:computed计算属性

---
+++ src/components/Compute.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>{{ name }}</h2>
+    </div>
+</template>
+
+<script setup>
+import { ref,reactive,computed } from "vue";
+const name = computed(()=>{
+    return '王二狗'
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
