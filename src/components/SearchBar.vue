c2a38bfabab26a4e1f582f1f12517fd0619fd2ae
XZ361 <2457307263@qq.com>
2023-10-18 07:47:30 +0800

    update:watch&watchEffect

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,12 +8,21 @@
 </template>
 
 <script setup>
-import { ref,reactive,computed } from "vue";
+import { ref,reactive,computed, watch, watchEffect } from "vue";
 let nums = ref([{ name: 'hhl' }, { name: '44sg' }, { name: '33i' }, { name: '44t' }]);
 let val = ref('')
 const searchNums = computed(()=>{
     // includes()：返回布尔值，表示是否找到了参数字符串。
     return nums.value.filter((item)=>item.name.includes(val.value))
+})
+
+// 监听属性
+// watch(val,(newVal,preVal)=>{
+//     console.log('val改变了！',newVal,preVal);
+// });
+// 初始化就会执行
+watchEffect(()=>{
+    console.log('watchEffect执行了',val.value);
 })
 </script>
 
